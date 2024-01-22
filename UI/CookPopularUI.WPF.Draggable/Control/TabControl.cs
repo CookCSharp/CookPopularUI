bf0a47d124177b5ad549d24cda7509514d6e9591
chance.zheng <465351405@qq.com>
2024-01-22 16:21:19 +0800

    add insert index for tabcontrol base on mouse position

--- UI/CookPopularUI.WPF.Draggable/Control/TabControl.cs
+++ UI/CookPopularUI.WPF.Draggable/Control/TabControl.cs
@@ -8,6 +8,7 @@
  */
 
 
+using CookPopularToolkit.Windows;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,16 +21,6 @@ using System.Windows.Input;
 
 namespace CookPopularUI.WPF.Draggable
 {
-    /// <summary>
-    /// This Data Provider represents TabItems.
-    /// Note that custom cursors are used.
-    /// When a TabItem is dragged within its
-    /// original container, the cursor is an arrow,
-    /// otherwise its a custom cursor with an
-    /// arrow and a page.
-    /// </summary>
-    /// <typeparam name="TContainer">Drag source container type</typeparam>
-    /// <typeparam name="TObject">Drag source object type</typeparam>
     public class TabControlDataProvider<TContainer, TObject> : DataProviderBase<TContainer, TObject>, IDataProvider
         where TContainer : ItemsControl
         where TObject : TabItem
@@ -52,7 +43,6 @@ namespace CookPopularUI.WPF.Draggable
             }
             else if (e.Effects == DragDropEffects.Link)
             {
-                // Drag tabs around
                 e.UseDefaultCursors = false;
                 Mouse.OverrideCursor = Cursors.Arrow;
                 e.Handled = true;
@@ -79,14 +69,6 @@ namespace CookPopularUI.WPF.Draggable
     }
 
 
-    /// <summary>
-    /// This data consumer looks for TabItems.
-    /// When the TabItem is dragged within its original
-    /// control, the TabItems are rearranged accordingly.
-    /// When dropped, it is inserted as the first TabItem.
-    /// </summary>
-    /// <typeparam name="TContainer">Drag source and drop destination container type</typeparam>
-    /// <typeparam name="TObject">Drag source and drop destination object type</typeparam>
     public class TabControlDataConsumer<TContainer, TObject> : DataConsumerBase, IDataConsumer
         where TContainer : ItemsControl
         where TObject : TabItem
@@ -102,67 +84,67 @@ namespace CookPopularUI.WPF.Draggable
 
         public override void DropTarget_Drop(object sender, System.Windows.DragEventArgs e) => DragOverOrDrop(true, sender, e);
 
-        /// <summary>
-        /// First determine whether the drag data is supported.
-        /// Next check if it's a move within the same TabControl,
-        /// and rearrange the TabItems.
-        /// Finally handle the actual drop when <code>bDrop</code> is true.
-        /// </summary>
-        /// <param name="bDrop">True to perform an actual drop, otherwise just return e.Effects</param>
-        /// <param name="sender">DragDrop event <code>sender</code></param>
-        /// <param name="e">DragDrop event arguments</param>
         private void DragOverOrDrop(bool bDrop, object sender, System.Windows.DragEventArgs e)
         {
             TabControlDataProvider<TContainer, TObject>? dataProvider = GetData(e) as TabControlDataProvider<TContainer, TObject>;
             if (dataProvider != null)
             {
                 TContainer? dragSourceContainer = dataProvider.SourceContainer as TContainer;
-                TObject? dragSourceObject = dataProvider.SourceObject as TObject;
-                Debug.Assert(dragSourceObject != null);
+                TObject? dragSource = dataProvider.SourceObject as TObject;
+                Debug.Assert(dragSource != null);
                 Debug.Assert(dragSourceContainer != null);
 
-                TContainer? dropContainer = Interop.GetParent<TContainer>(e.Source as DependencyObject);
+                TContainer? dropTargetContainer = Interop.GetParent<TContainer>(e.Source as DependencyObject);
                 TObject? dropTarget = e.Source as TObject;
 
-                if (dropContainer != null && dragSourceContainer == dropContainer && dropTarget != null)
+                if (dragSource != null && dropTarget != null && dragSourceContainer != null && dropTargetContainer != null && dragSourceContainer == dropTargetContainer)
                 {
-                    int srcIndex = dragSourceContainer.Items.IndexOf(dragSourceObject);
-                    int dstIndex = dropContainer.Items.IndexOf(dropTarget);
+                    int srcIndex = dragSourceContainer.Items.IndexOf(dragSource);
+                    int dstIndex = dropTargetContainer.Items.IndexOf(dropTarget);
+
                     if (srcIndex != dstIndex)
                     {
-                        // Only move when there's no chance of oscillation
                         bool doMove = true;
-                        if (dragSourceObject!.ActualWidth < (dropTarget.ActualWidth))
+                        if (dragSource.ActualWidth < (dropTarget.ActualWidth))
                         {
                             Point point = e.GetPosition(dropTarget);
                             if (srcIndex < dstIndex)
                             {
-                                doMove = point.X > dropTarget.ActualWidth - dragSourceObject.ActualWidth;
+                                doMove = point.X > dropTarget.ActualWidth - dragSource.ActualWidth;
                             }
                             else
                             {
-                                doMove = point.X < dragSourceObject.ActualWidth;
+                                doMove = point.X < dragSource.ActualWidth;
                             }
                         }
                         if (doMove)
                         {
                             dataProvider.Unparent();
-                            dropContainer.Items.Insert(dstIndex, dragSourceObject);
-                            dragSourceObject.IsSelected = true;
+                            dropTargetContainer.Items.Insert(dstIndex, dragSource);
+                            dragSource.IsSelected = true;
                         }
                     }
+
                     e.Effects = DragDropEffects.Link;
                     e.Handled = true;
                 }
-                else if (dropContainer != null)
+                else if (dragSource != null && dropTarget != null && dragSourceContainer != null && dropTargetContainer != null && dragSourceContainer != dropTargetContainer)
                 {
-                    // 拖动到指定位置
                     if (bDrop)
                     {
                         dataProvider.Unparent();
-                        dropContainer.Items.Insert(dropContainer.Items.IndexOf(dropTarget), dragSourceObject);
-                        dragSourceObject!.IsSelected = true;
+
+                        int insertIndex = dropTargetContainer.Items.IndexOf(dropTarget);
+                        var position = e.GetPosition(dropTargetContainer);
+                        var item = dropTargetContainer.GetItemContainerAt(position);
+                        var currentPosion = e.GetPosition(item);
+                        GetIndex(dropTargetContainer, item, currentPosion, ref insertIndex);
+
+                        dropTargetContainer.Items.Insert(insertIndex, dragSource);
+
+                        dragSource.IsSelected = true;
                     }
+
                     e.Effects = DragDropEffects.Move;
                     e.Handled = true;
                 }
@@ -172,6 +154,37 @@ namespace CookPopularUI.WPF.Draggable
                     e.Handled = true;
                 }
             }
+        }
+
+        private int GetIndex(TContainer dropTargetContainer, UIElement? element, Point point, ref int index)
+        {
+            if (dropTargetContainer == null || element == null)
+                return index;
+
+            var currentXPos = point.X;
+            var currentYPos = point.Y;
+            var targetWidth = element.RenderSize.Width;
+            var targetHeight = element.RenderSize.Height;
+
+            if (dropTargetContainer is TabControl tab)
+            {
+                if (tab.TabStripPlacement == Dock.Top || tab.TabStripPlacement == Dock.Bottom)
+                {
+                    if (currentXPos > targetWidth / 2)
+                    {
+                        index += 1;
+                    }
+                }
+                else
+                {
+                    if (currentYPos > targetHeight / 2)
+                    {
+                        index += 1;
+                    }
+                }
+            }
+
+            return index;
         }
     }
 }
