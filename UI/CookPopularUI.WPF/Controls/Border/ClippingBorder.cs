a3b08180089c62018cc3308ea54dd998b023adfd
chance.zheng <465351405@qq.com>
2024-01-31 16:10:37 +0800

    add dropshadowborder

--- UI/CookPopularUI.WPF/Controls/Border/ClippingBorder.cs
+++ UI/CookPopularUI.WPF/Controls/Border/ClippingBorder.cs
@@ -24,15 +24,14 @@ namespace CookPopularUI.WPF.Controls
     /// </summary>
     public class ClippingBorder : ContentControl
     {
-        private ContentControl? topLeftContentControl;
-        private ContentControl? topRightContentControl;
-        private ContentControl? bottomRightContentControl;
-        private ContentControl? bottomLeftContentControl;
-
-        private RectangleGeometry? topLeftClip;
-        private RectangleGeometry? topRightClip;
-        private RectangleGeometry? bottomRightClip;
-        private RectangleGeometry? bottomLeftClip;
+        private ContentControl? _topLeftContentControl;
+        private ContentControl? _topRightContentControl;
+        private ContentControl? _bottomRightContentControl;
+        private ContentControl? _bottomLeftContentControl;
+        private RectangleGeometry? _topLeftClip;
+        private RectangleGeometry? _topRightClip;
+        private RectangleGeometry? _bottomRightClip;
+        private RectangleGeometry? _bottomLeftClip;
 
         private Border? _border;
 
@@ -80,20 +79,20 @@ namespace CookPopularUI.WPF.Controls
             base.OnApplyTemplate();
 
             _border = GetTemplateChild("PART_Border") as Border;
-            topLeftContentControl = GetTemplateChild("PART_TopLeftContentControl") as ContentControl;
-            topRightContentControl = GetTemplateChild("PART_TopRightContentControl") as ContentControl;
-            bottomRightContentControl = GetTemplateChild("PART_BottomRightContentControl") as ContentControl;
-            bottomLeftContentControl = GetTemplateChild("PART_BottomLeftContentControl") as ContentControl;
+            _topLeftContentControl = GetTemplateChild("PART_TopLeftContentControl") as ContentControl;
+            _topRightContentControl = GetTemplateChild("PART_TopRightContentControl") as ContentControl;
+            _bottomRightContentControl = GetTemplateChild("PART_BottomRightContentControl") as ContentControl;
+            _bottomLeftContentControl = GetTemplateChild("PART_BottomLeftContentControl") as ContentControl;
 
-            if (topLeftContentControl != null)
+            if (_topLeftContentControl != null)
             {
-                topLeftContentControl.SizeChanged += new SizeChangedEventHandler(ContentControl_SizeChanged);
+                _topLeftContentControl.SizeChanged += new SizeChangedEventHandler(ContentControl_SizeChanged);
             }
 
-            topLeftClip = GetTemplateChild("PART_TopLeftClip") as RectangleGeometry;
-            topRightClip = GetTemplateChild("PART_TopRightClip") as RectangleGeometry;
-            bottomRightClip = GetTemplateChild("PART_BottomRightClip") as RectangleGeometry;
-            bottomLeftClip = GetTemplateChild("PART_BottomLeftClip") as RectangleGeometry;
+            _topLeftClip = GetTemplateChild("PART_TopLeftClip") as RectangleGeometry;
+            _topRightClip = GetTemplateChild("PART_TopRightClip") as RectangleGeometry;
+            _bottomRightClip = GetTemplateChild("PART_BottomRightClip") as RectangleGeometry;
+            _bottomLeftClip = GetTemplateChild("PART_BottomLeftClip") as RectangleGeometry;
 
             UpdateClipContent(IsClipContent);
             UpdateCornerRadius(CornerRadius);
@@ -106,24 +105,24 @@ namespace CookPopularUI.WPF.Controls
                 _border.CornerRadius = newCornerRadius;
             }
 
-            if (topLeftClip != null)
+            if (_topLeftClip != null)
             {
-                topLeftClip.RadiusX = topLeftClip.RadiusY = newCornerRadius.TopLeft - (Math.Min(BorderThickness.Left, BorderThickness.Top) / 2);
+                _topLeftClip.RadiusX = _topLeftClip.RadiusY = newCornerRadius.TopLeft - (Math.Min(BorderThickness.Left, BorderThickness.Top) / 2);
             }
 
-            if (topRightClip != null)
+            if (_topRightClip != null)
             {
-                topRightClip.RadiusX = topRightClip.RadiusY = newCornerRadius.TopRight - (Math.Min(BorderThickness.Top, BorderThickness.Right) / 2);
+                _topRightClip.RadiusX = _topRightClip.RadiusY = newCornerRadius.TopRight - (Math.Min(BorderThickness.Top, BorderThickness.Right) / 2);
             }
 
-            if (bottomRightClip != null)
+            if (_bottomRightClip != null)
             {
-                bottomRightClip.RadiusX = bottomRightClip.RadiusY = newCornerRadius.BottomRight - (Math.Min(BorderThickness.Right, BorderThickness.Bottom) / 2);
+                _bottomRightClip.RadiusX = _bottomRightClip.RadiusY = newCornerRadius.BottomRight - (Math.Min(BorderThickness.Right, BorderThickness.Bottom) / 2);
             }
 
-            if (bottomLeftClip != null)
+            if (_bottomLeftClip != null)
             {
-                bottomLeftClip.RadiusX = bottomLeftClip.RadiusY = newCornerRadius.BottomLeft - (Math.Min(BorderThickness.Bottom, BorderThickness.Left) / 2);
+                _bottomLeftClip.RadiusX = _bottomLeftClip.RadiusY = newCornerRadius.BottomLeft - (Math.Min(BorderThickness.Bottom, BorderThickness.Left) / 2);
             }
 
             UpdateClipSize(new Size(ActualWidth, ActualHeight));
@@ -133,48 +132,48 @@ namespace CookPopularUI.WPF.Controls
         {
             if (isClipContent)
             {
-                if (topLeftContentControl != null)
+                if (_topLeftContentControl != null)
                 {
-                    topLeftContentControl.Clip = topLeftClip;
+                    _topLeftContentControl.Clip = _topLeftClip;
                 }
 
-                if (topRightContentControl != null)
+                if (_topRightContentControl != null)
                 {
-                    topRightContentControl.Clip = topRightClip;
+                    _topRightContentControl.Clip = _topRightClip;
                 }
 
-                if (bottomRightContentControl != null)
+                if (_bottomRightContentControl != null)
                 {
-                    bottomRightContentControl.Clip = bottomRightClip;
+                    _bottomRightContentControl.Clip = _bottomRightClip;
                 }
 
-                if (bottomLeftContentControl != null)
+                if (_bottomLeftContentControl != null)
                 {
-                    bottomLeftContentControl.Clip = bottomLeftClip;
+                    _bottomLeftContentControl.Clip = _bottomLeftClip;
                 }
 
                 UpdateClipSize(new Size(ActualWidth, ActualHeight));
             }
             else
             {
-                if (topLeftContentControl != null)
+                if (_topLeftContentControl != null)
                 {
-                    topLeftContentControl.Clip = null;
+                    _topLeftContentControl.Clip = null;
                 }
 
-                if (topRightContentControl != null)
+                if (_topRightContentControl != null)
                 {
-                    topRightContentControl.Clip = null;
+                    _topRightContentControl.Clip = null;
                 }
 
-                if (bottomRightContentControl != null)
+                if (_bottomRightContentControl != null)
                 {
-                    bottomRightContentControl.Clip = null;
+                    _bottomRightContentControl.Clip = null;
                 }
 
-                if (bottomLeftContentControl != null)
+                if (_bottomLeftContentControl != null)
                 {
-                    bottomLeftContentControl.Clip = null;
+                    _bottomLeftContentControl.Clip = null;
                 }
             }
         }
@@ -202,24 +201,24 @@ namespace CookPopularUI.WPF.Controls
                 double contentWidth = Math.Max(0, size.Width - BorderThickness.Left - BorderThickness.Right);
                 double contentHeight = Math.Max(0, size.Height - BorderThickness.Top - BorderThickness.Bottom);
 
-                if (topLeftClip != null)
+                if (_topLeftClip != null)
                 {
-                    topLeftClip.Rect = new Rect(0, 0, contentWidth + (CornerRadius.TopLeft * 2), contentHeight + (CornerRadius.TopLeft * 2));
+                    _topLeftClip.Rect = new Rect(0, 0, contentWidth + (CornerRadius.TopLeft * 2), contentHeight + (CornerRadius.TopLeft * 2));
                 }
 
-                if (topRightClip != null)
+                if (_topRightClip != null)
                 {
-                    topRightClip.Rect = new Rect(0 - CornerRadius.TopRight, 0, contentWidth + CornerRadius.TopRight, contentHeight + CornerRadius.TopRight);
+                    _topRightClip.Rect = new Rect(0 - CornerRadius.TopRight, 0, contentWidth + CornerRadius.TopRight, contentHeight + CornerRadius.TopRight);
                 }
 
-                if (bottomRightClip != null)
+                if (_bottomRightClip != null)
                 {
-                    bottomRightClip.Rect = new Rect(0 - CornerRadius.BottomRight, 0 - CornerRadius.BottomRight, contentWidth + CornerRadius.BottomRight, contentHeight + CornerRadius.BottomRight);
+                    _bottomRightClip.Rect = new Rect(0 - CornerRadius.BottomRight, 0 - CornerRadius.BottomRight, contentWidth + CornerRadius.BottomRight, contentHeight + CornerRadius.BottomRight);
                 }
 
-                if (bottomLeftClip != null)
+                if (_bottomLeftClip != null)
                 {
-                    bottomLeftClip.Rect = new Rect(0, 0 - CornerRadius.BottomLeft, contentWidth + CornerRadius.BottomLeft, contentHeight + CornerRadius.BottomLeft);
+                    _bottomLeftClip.Rect = new Rect(0, 0 - CornerRadius.BottomLeft, contentWidth + CornerRadius.BottomLeft, contentHeight + CornerRadius.BottomLeft);
                 }
             }
         }
---
+++ UI/CookPopularUI.WPF/Controls/Border/DropShadowBorder.cs
@@ -0,0 +1,241 @@
+﻿/*
+ *Description: DropShadowBorder
+ *Author: Chance.zheng
+ *Creat Time: 2024/1/31 14:53:15
+ *.Net Version: 4.6
+ *CLR Version: 4.0.30319.42000
+ *Copyright © CookCSharp 2024 All Rights Reserved.
+ */
+
+
+using CookPopularToolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CookPopularUI.WPF.Controls
+{
+    public class DropShadowBorder : ContentControl
+    {
+        private GradientStop _shadowHorizontal1;
+        private GradientStop _shadowHorizontal2;
+        private GradientStop _shadowVertical1;
+        private GradientStop _shadowVertical2;
+        private GradientStop _shadowOuterStop1;
+        private GradientStop _shadowOuterStop2;
+        private TranslateTransform _dropShadowTranslate;
+
+
+        [System.ComponentModel.Category("Appearance"), System.ComponentModel.Description("the drop shadow color.")]
+        public Color DropShadowColor
+        {
+            get => (Color)GetValue(DropShadowColorProperty);
+            set => SetValue(DropShadowColorProperty, value);
+        }
+        public static readonly DependencyProperty DropShadowColorProperty =
+            DependencyProperty.Register(nameof(DropShadowColor), typeof(Color), typeof(DropShadowBorder), new PropertyMetadata(new PropertyChangedCallback(OnDropShadowColorChanged)));
+
+        private static void OnDropShadowColorChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            if (dependencyObject is DropShadowBorder dropShadowBorder)
+                dropShadowBorder.UpdateDropShadowColor((Color)eventArgs.NewValue);
+        }
+
+
+        [System.ComponentModel.Category("Appearance"), System.ComponentModel.Description("The drop shadow opacity.")]
+        public double DropShadowOpacity
+        {
+            get { return (double)GetValue(DropShadowOpacityProperty); }
+            set { SetValue(DropShadowOpacityProperty, value); }
+        }
+        public static readonly DependencyProperty DropShadowOpacityProperty =
+            DependencyProperty.Register(nameof(DropShadowOpacity), typeof(double), typeof(DropShadowBorder), null);
+
+
+        /// <summary>
+        /// 阴影距离
+        /// </summary>
+        [System.ComponentModel.Category("Appearance"), System.ComponentModel.Description("The drop shadow distance.")]
+        public double DropShadowDistance
+        {
+            get { return (double)GetValue(DropShadowDistanceProperty); }
+            set { SetValue(DropShadowDistanceProperty, value); }
+        }
+        public static readonly DependencyProperty DropShadowDistanceProperty =
+            DependencyProperty.Register(nameof(DropShadowDistance), typeof(double), typeof(DropShadowBorder), new PropertyMetadata(new PropertyChangedCallback(OnDropShadowDistanceChanged)));
+
+        private static void OnDropShadowDistanceChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            if (dependencyObject is DropShadowBorder dropShadowBorder)
+                dropShadowBorder.UpdateDropShadowPosition();
+        }
+
+
+        /// <summary>
+        /// 阴影角度
+        /// </summary>
+        [System.ComponentModel.Category("Appearance"), System.ComponentModel.Description("The drop shadow angle.")]
+        public double DropShadowAngle
+        {
+            get { return (double)GetValue(DropShadowAngleProperty); }
+            set { SetValue(DropShadowAngleProperty, value); }
+        }
+        public static readonly DependencyProperty DropShadowAngleProperty =
+            DependencyProperty.Register(nameof(DropShadowAngle), typeof(double), typeof(DropShadowBorder), new PropertyMetadata(new PropertyChangedCallback(OnDropShadowAngleChanged)));
+
+        private static void OnDropShadowAngleChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            if (dependencyObject is DropShadowBorder dropShadowBorder)
+                dropShadowBorder.UpdateDropShadowPosition();
+        }
+
+
+        /// <summary>
+        /// 阴影扩展
+        /// </summary>
+        [System.ComponentModel.Category("Appearance"), System.ComponentModel.Description("The drop shadow spread.")]
+        public double DropShadowSpread
+        {
+            get { return (double)GetValue(DropShadowSpreadProperty); }
+            set { SetValue(DropShadowSpreadProperty, value); }
+        }
+        public static readonly DependencyProperty DropShadowSpreadProperty =
+            DependencyProperty.Register(nameof(DropShadowSpread), typeof(double), typeof(DropShadowBorder), new PropertyMetadata(new PropertyChangedCallback(OnDropShadowSpreadChanged)));
+
+        private static void OnDropShadowSpreadChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            if (dependencyObject is DropShadowBorder dropShadowBorder)
+                dropShadowBorder.UpdateStops(new Size(dropShadowBorder.ActualWidth, dropShadowBorder.ActualHeight));
+        }
+
+
+
+        [System.ComponentModel.Category("Appearance"), System.ComponentModel.Description("Sets the corner radius on the border.")]
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+
+            set
+            {
+                SetValue(CornerRadiusProperty, value);
+
+                CornerRadius shadowCornerRadius = new CornerRadius(Math.Abs(value.TopLeft * 1.5),
+                                                                   Math.Abs(value.TopRight * 1.5),
+                                                                   Math.Abs(value.BottomRight * 1.5),
+                                                                   Math.Abs(value.BottomLeft * 1.5));
+                SetValue(ShadowCornerRadiusProperty, shadowCornerRadius);
+            }
+        }
+        public static readonly DependencyProperty CornerRadiusProperty =
+            DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(DropShadowBorder), null);
+
+
+        public double ShadowCornerRadius
+        {
+            get { return (double)GetValue(ShadowCornerRadiusProperty); }
+        }
+        public static readonly DependencyProperty ShadowCornerRadiusProperty =
+            DependencyProperty.Register(nameof(ShadowCornerRadius), typeof(CornerRadius), typeof(DropShadowBorder), null);
+
+
+        [System.ComponentModel.Category("Appearance"), System.ComponentModel.Description("Sets whether the content is clipped or not.")]
+        public bool IsClipContent
+        {
+            get { return (bool)GetValue(IsClipContentProperty); }
+            set { SetValue(IsClipContentProperty, value); }
+        }
+        public static readonly DependencyProperty IsClipContentProperty =
+            DependencyProperty.Register(nameof(IsClipContent), typeof(bool), typeof(DropShadowBorder), null);
+
+
+        public DropShadowBorder()
+        {
+            DefaultStyleKey = typeof(DropShadowBorder);
+            SizeChanged += (s, e) => UpdateStops(e.NewSize);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _dropShadowTranslate = (TranslateTransform)GetTemplateChild("PART_DropShadowTranslateTransform");
+            _shadowOuterStop1 = (GradientStop)GetTemplateChild("PART_ShadowOuterStop1");
+            _shadowOuterStop2 = (GradientStop)GetTemplateChild("PART_ShadowOuterStop2");
+            _shadowVertical1 = (GradientStop)GetTemplateChild("PART_ShadowVertical1");
+            _shadowVertical2 = (GradientStop)GetTemplateChild("PART_ShadowVertical2");
+            _shadowHorizontal1 = (GradientStop)GetTemplateChild("PART_ShadowHorizontal1");
+            _shadowHorizontal2 = (GradientStop)GetTemplateChild("PART_ShadowHorizontal2");
+
+            UpdateDropShadowPosition();
+            UpdateStops(new Size(ActualWidth, ActualHeight));
+        }
+
+        private void UpdateDropShadowPosition()
+        {
+            if (_dropShadowTranslate != null)
+            {
+                double x = Math.Cos(DropShadowAngle * (Math.PI / 180)) * DropShadowDistance;
+                double y = Math.Tan(DropShadowAngle * (Math.PI / 180)) * x;
+                Point offset = new Point(x, y);
+
+                _dropShadowTranslate.X = offset.X;
+                _dropShadowTranslate.Y = offset.Y;
+            }
+        }
+
+        private void UpdateStops(Size size)
+        {
+            if (size.Width > 0 && size.Height > 0)
+            {
+                if (_shadowHorizontal1 != null)
+                {
+                    _shadowHorizontal1.Offset = DropShadowSpread / (size.Width + (DropShadowSpread * 2));
+                }
+
+                if (_shadowHorizontal2 != null)
+                {
+                    _shadowHorizontal2.Offset = 1 - (DropShadowSpread / (size.Width + (DropShadowSpread * 2)));
+                }
+
+                if (_shadowVertical1 != null)
+                {
+                    _shadowVertical1.Offset = DropShadowSpread / (size.Height + (DropShadowSpread * 2));
+                }
+
+                if (_shadowVertical2 != null)
+                {
+                    _shadowVertical2.Offset = 1 - (DropShadowSpread / (size.Height + (DropShadowSpread * 2)));
+                }
+            }
+        }
+
+        private void UpdateDropShadowColor(Color color)
+        {
+            if (_shadowVertical1 != null)
+            {
+                _shadowVertical1.Color = color;
+            }
+
+            if (_shadowVertical2 != null)
+            {
+                _shadowVertical2.Color = color;
+            }
+
+            if (_shadowOuterStop1 != null)
+            {
+                _shadowOuterStop1.Color = Color.FromArgb(0, color.R, color.G, color.B);
+            }
+
+            if (_shadowOuterStop2 != null)
+            {
+                _shadowOuterStop2.Color = Color.FromArgb(0, color.R, color.G, color.B);
+            }
+        }
+    }
+}
